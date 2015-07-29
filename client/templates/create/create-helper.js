25490efea181f9c9c26a45f28fb825fc18733fbd
Lundell <Lundelll@gmail.com>
2015-07-29 14:28:03 +0200

    Some additions on the create page

--- client/templates/create/create-helper.js
+++ client/templates/create/create-helper.js
@@ -1,6 +1,11 @@
 if (Meteor.isClient) {
+
+    var teamList = [];
+
     Template.create.helpers({
-        teamList: []
+        showTeamList: function () {
+            // ToDo: Fix this so when a team is added to the list, present it on the page.
+        }
     });
 
     Template.create.events({
@@ -10,10 +15,37 @@ if (Meteor.isClient) {
             var teamName = document.getElementById('txtAddTeam');
 
             if (teamName.value !== '') {
-                console.log(teamName.value);
+                $('.error-msg-addTeam').text('');
+                console.log('Added team: ' + teamName.value);
+                teamList.push(teamName.value);
+                console.log('teamList length = ' + teamList.length);
                 document.getElementById('txtAddTeam').value = '';
+                if (teamList.length > 0) {
+                    for (var i = 0; i < teamList.length; i++) {
+                        console.log(i + ". " + teamList[i]);
+                    }
+                }
             } else {
-                console.log('LOL! Silly human, you need a name on your team!');
+                $('.error-msg-addTeam').text('You need a name for the team you want to add.');
+            }
+        },
+
+        'submit .team-list': function (event) {
+            event.preventDefault();
+
+            var tournamentName = event.target.tournamentName.value;
+
+            if (teamList.length >= 2 && tournamentName) {
+                var tournamentInfo = [tournamentName, teamList];
+                // ToDo: fix this on the server.
+                Meteor.call("createTournament", tournamentInfo);
+                event.target.tournamentName.value = '';
+            } else {
+                if (teamList.length < 2) {
+                    $('.error-msg-addTeam').text('You need at least 2 teams to create a tournament.');
+                } else if (!tournamentName) {
+                    $('.error-msg-tournamentName').text('You need a name for your tournament.');
+                }
             }
         }
     });
